f7bfab57268898fd589838efcdb57714bc66f758
talhajilal@gmail.com <root@ip-172-31-26-65.us-west-2.compute.internal>
2017-06-22 15:49:54 -0400

    New python script files

---
+++ add_new_git_file.sh
@@ -0,0 +1,15 @@
+origin=$(git remote -v | grep push | awk '{print $2}')
+echo  "Your new changes in this directory will go to this origin $origin"
+result=$(echo $?)
+if [ "$result" -eq 0 ]; then
+echo "Proceeding to commit new file"
+else 
+echo "No git hub origin define" 
+exit
+fi
+echo "Please enter your commit msg here"
+read msg
+git init .
+git add . 
+git commit -m "$msg"
+git push  
